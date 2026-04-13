158ad47358e28595e711abcd68ab64905ad49413
xushen1986 <xushen1986@gmail.com>
2026-04-13 12:40:44 +0100

    Add Rainbow cat art project

--- projects.js
+++ projects.js
@@ -1,5 +1,13 @@
 const scratchProjects = [
   {
+    id: "scratch-1304812070",
+    title: "Rainbow cat art",
+    summary: "Astra's first Scratch project",
+    tag: "Scratch",
+    embedHtml:
+      '<iframe src="https://scratch.mit.edu/projects/1304812070/embed" allowtransparency="true" width="485" height="402" frameborder="0" scrolling="no" allowfullscreen></iframe>'
+  },
+  {
     id: "scratch-1306323320",
     title: "Happy birthday!",
     summary: "A shared Scratch creation by Astra, now showcased under its original Scratch name.",
