688d2bb1acb10dfc0f4142efa3388656ca1dd5fa
xushen1986 <xushen1986@gmail.com>
2026-04-13 12:44:39 +0100

    Add Go go go project

--- projects.js
+++ projects.js
@@ -1,5 +1,13 @@
 const scratchProjects = [
   {
+    id: "scratch-1305462358",
+    title: "Go go go",
+    summary: "Let's race! Use \"z\" and \"x\" for the bat and \"n\" and \"m\" for the dog.",
+    tag: "Scratch",
+    embedHtml:
+      '<iframe src="https://scratch.mit.edu/projects/1305462358/embed" allowtransparency="true" width="485" height="402" frameborder="0" scrolling="no" allowfullscreen></iframe>'
+  },
+  {
     id: "scratch-1305026507",
     title: "Dancing dinosaur",
     summary:
