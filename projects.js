3ae46afbf607493494665f3ad5afd52224eb161a
xushen1986 <109353681+xushen1986@users.noreply.github.com>
2026-04-14 15:42:28 +0100

    Add Rainbow spin project

--- projects.js
+++ projects.js
@@ -1,67 +1,65 @@
 const scratchProjects = [
   {
+    id: "scratch-1306908864",
+    title: "Rainbow spin",
+    summary: "Look how I spin and follow you around.\nYou can press up and down key to show the variables to change how I move.",
+    tag: "Scratch",
+    embedHtml: "\u003ciframe src=\"https://scratch.mit.edu/projects/1306908864/embed\" allowtransparency=\"true\" width=\"485\" height=\"402\" frameborder=\"0\" scrolling=\"no\" allowfullscreen\u003e\u003c/iframe\u003e"
+  },
+  {
     id: "scratch-1306436699",
     title: "Doubling numbers",
     summary: "Tell me a number and I can keep doubling it. Just keep pressing \"space\" key.",
     tag: "Scratch",
-    embedHtml:
-      '<iframe src="https://scratch.mit.edu/projects/1306436699/embed" allowtransparency="true" width="485" height="402" frameborder="0" scrolling="no" allowfullscreen></iframe>'
+    embedHtml: "\u003ciframe src=\"https://scratch.mit.edu/projects/1306436699/embed\" allowtransparency=\"true\" width=\"485\" height=\"402\" frameborder=\"0\" scrolling=\"no\" allowfullscreen\u003e\u003c/iframe\u003e"
   },
   {
     id: "scratch-1306013263",
     title: "Monster faces",
     summary: "Let us make a funny face!",
     tag: "Scratch",
-    embedHtml:
-      '<iframe src="https://scratch.mit.edu/projects/1306013263/embed" allowtransparency="true" width="485" height="402" frameborder="0" scrolling="no" allowfullscreen></iframe>'
+    embedHtml: "\u003ciframe src=\"https://scratch.mit.edu/projects/1306013263/embed\" allowtransparency=\"true\" width=\"485\" height=\"402\" frameborder=\"0\" scrolling=\"no\" allowfullscreen\u003e\u003c/iframe\u003e"
   },
   {
     id: "scratch-1305803578",
     title: "Galloping horse",
     summary: "Ask me any number questions.",
     tag: "Scratch",
-    embedHtml:
-      '<iframe src="https://scratch.mit.edu/projects/1305803578/embed" allowtransparency="true" width="485" height="402" frameborder="0" scrolling="no" allowfullscreen></iframe>'
+    embedHtml: "\u003ciframe src=\"https://scratch.mit.edu/projects/1305803578/embed\" allowtransparency=\"true\" width=\"485\" height=\"402\" frameborder=\"0\" scrolling=\"no\" allowfullscreen\u003e\u003c/iframe\u003e"
   },
   {
     id: "scratch-1305793098",
     title: "Fortune teller",
     summary: "Ask me any yes-or-no questions.",
     tag: "Scratch",
-    embedHtml:
-      '<iframe src="https://scratch.mit.edu/projects/1305793098/embed" allowtransparency="true" width="485" height="402" frameborder="0" scrolling="no" allowfullscreen></iframe>'
+    embedHtml: "\u003ciframe src=\"https://scratch.mit.edu/projects/1305793098/embed\" allowtransparency=\"true\" width=\"485\" height=\"402\" frameborder=\"0\" scrolling=\"no\" allowfullscreen\u003e\u003c/iframe\u003e"
   },
   {
     id: "scratch-1305462358",
     title: "Go go go",
     summary: "Let's race! Use \"z\" and \"x\" for the bat and \"n\" and \"m\" for the dog.",
     tag: "Scratch",
-    embedHtml:
-      '<iframe src="https://scratch.mit.edu/projects/1305462358/embed" allowtransparency="true" width="485" height="402" frameborder="0" scrolling="no" allowfullscreen></iframe>'
+    embedHtml: "\u003ciframe src=\"https://scratch.mit.edu/projects/1305462358/embed\" allowtransparency=\"true\" width=\"485\" height=\"402\" frameborder=\"0\" scrolling=\"no\" allowfullscreen\u003e\u003c/iframe\u003e"
   },
   {
     id: "scratch-1305026507",
     title: "Dancing dinosaur",
-    summary:
-      "Let's party! Use left and right key to control one of the dinosaurs, and press \"x\" to make they shout \"Party!\"",
+    summary: "Let's party! Use left and right key to control one of the dinosaurs, and press \"x\" to make they shout \"Party!\"",
     tag: "Scratch",
-    embedHtml:
-      '<iframe src="https://scratch.mit.edu/projects/1305026507/embed" allowtransparency="true" width="485" height="402" frameborder="0" scrolling="no" allowfullscreen></iframe>'
+    embedHtml: "\u003ciframe src=\"https://scratch.mit.edu/projects/1305026507/embed\" allowtransparency=\"true\" width=\"485\" height=\"402\" frameborder=\"0\" scrolling=\"no\" allowfullscreen\u003e\u003c/iframe\u003e"
   },
   {
     id: "scratch-1304812070",
     title: "Rainbow cat art",
     summary: "Astra's first Scratch project",
     tag: "Scratch",
-    embedHtml:
-      '<iframe src="https://scratch.mit.edu/projects/1304812070/embed" allowtransparency="true" width="485" height="402" frameborder="0" scrolling="no" allowfullscreen></iframe>'
+    embedHtml: "\u003ciframe src=\"https://scratch.mit.edu/projects/1304812070/embed\" allowtransparency=\"true\" width=\"485\" height=\"402\" frameborder=\"0\" scrolling=\"no\" allowfullscreen\u003e\u003c/iframe\u003e"
   },
   {
     id: "scratch-1306323320",
     title: "Happy birthday!",
     summary: "Turn the sound on. :) Astra wants to wish her friends a big happy birthday!",
     tag: "Scratch",
-    embedHtml:
-      '<iframe src="https://scratch.mit.edu/projects/1306323320/embed" allowtransparency="true" width="485" height="402" frameborder="0" scrolling="no" allowfullscreen></iframe>'
+    embedHtml: "\u003ciframe src=\"https://scratch.mit.edu/projects/1306323320/embed\" allowtransparency=\"true\" width=\"485\" height=\"402\" frameborder=\"0\" scrolling=\"no\" allowfullscreen\u003e\u003c/iframe\u003e"
   }
 ];
